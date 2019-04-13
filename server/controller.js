70284270aff215ca0dae74a1b24845eae6aca31a
Tim Luu <timbo.luu@gmail.com>
2019-04-13 15:30:20 -0700

    Separate cutString functionality from exported function

--- server/controller.js
+++ server/controller.js
@@ -1,10 +1,22 @@
-module.exports = {
-  cutString: function(str) {
-    let reducedStr = '';
+let cutString = (str) => {
+  let reducedStr = '';
     for(let i = 2; i < str.length; i+= 3) {
       reducedStr += str[i];
     }
 
     return reducedStr;
+}
+
+
+module.exports = {
+  cut: function(str) {
+    return new Promise((resolve, reject) => {
+      let reducedStr = cutString(str);
+      if(reducedStr) {
+        resolve(reducedStr);
+      } else {
+        reject(new Error('There appears to be an error with your string.'));
+      }
+    })
   }
 }
