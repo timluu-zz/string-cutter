8c189a54a6eecf35017fd84f24c7194e692cb8f7
Tim Luu <timbo.luu@gmail.com>
2019-04-13 15:33:00 -0700

    Connect controller methods to post route

--- server/index.js
+++ server/index.js
@@ -1,13 +1,20 @@
-const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const controller = require('./controller.js');
 
 const port = 3000;
 app.use(bodyParser.json());
 
 app.post('/test', (req, res) => {
-  let str = req.body;
-  console.log(str);
+  let str = req.body['string_to_cut'];
+  controller.cut(str)
+    .then((response) => {
+      res.status(201).send(response);
+    })
+    .catch((error) => {
+      res.status(400).send(error);
+    })
 })
 
 app.listen(port, () => {
