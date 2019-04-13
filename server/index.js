652c8b627db1ba524c4b6ef7d2f8afd3fef4a956
Tim Luu <timbo.luu@gmail.com>
2019-04-13 15:55:34 -0700

    Serve static files

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const bodyParser = require('body-parser');
 const controller = require('./controller.js');
 
 const port = 3000;
 app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.post('/test', (req, res) => {
   let str = req.body['string_to_cut'];
