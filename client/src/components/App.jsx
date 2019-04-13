9b12dfa4b0f879aaaefbffee62ec325dfab72704
Tim Luu <timbo.luu@gmail.com>
2019-04-13 16:49:01 -0700

    Add event handlers

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import EntryForm from './EntryForm.jsx';
 
 class App extends React.Component {
@@ -11,9 +12,39 @@ class App extends React.Component {
     }
   }
 
+  handleChange(e) {
+    this.setState({
+      str: e.target.value
+    })
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.submitString(this.state.str);
+  }
+
+  submitString(str) {
+    axios.post('/test', {
+      string_to_cut: this.state.str
+    })
+      .then((response) => {
+        this.setState({
+          cutStr: response.data['return_string'],
+          str: ''
+        })
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }
+
   render() {
     return (
-      <EntryForm />
+      <EntryForm 
+        change={this.handleChange.bind(this)}
+        submit={this.handleSubmit.bind(this)}
+        value={this.state.str}
+      />
     )
   }
 }
