033188120d364e18817c9a50953a05a36fbfd6d1
Tim Luu <timbo.luu@gmail.com>
2019-04-13 18:47:19 -0700

    Import BannerStyles

---
+++ client/src/components/Banner.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Header } from '../styles/BannerStyles.jsx';
+
+const Banner = () => {
+  return (
+    <Header>
+      String Cutter
+    </Header>
+  )
+}
+
+export default Banner;
