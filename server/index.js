1bb54a6b3eb85e2825fd38118adf91cc3b95247f
Tim Luu <timbo.luu@gmail.com>
2019-04-16 14:51:15 -0700

    Refactor cut method to controller file

--- server/index.js
+++ server/index.js
@@ -1,27 +1,17 @@
 const express = require('express');
 const app = express();
-const path = require('path');
 const bodyParser = require('body-parser');
+const { body } = require('express-validator/check');
 const controller = require('./controller.js');
+const config = require('../config.js');
 
-const port = 3000;
+const port = config.port;
 app.use(bodyParser.json());
+app.use(express.static(config.staticAssets));
 
-app.use(express.static(path.join(__dirname, '../client/dist')));
-
-app.post('/test', (req, res) => {
-  let str = req.body['string_to_cut'];
-  controller.cut(str)
-    .then((response) => {
-      let data = {
-        return_string: response
-      };
-      res.status(201).send(data);
-    })
-    .catch((error) => {
-      res.status(400).send(error);
-    })
-})
+app.post('/test', [
+  body('string_to_cut', 'Input needs to be a valid string').isString()
+], controller.cut);
 
 app.listen(port, () => {
   console.log(`currently listening to port ${port}`);
