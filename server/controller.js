c7392a2353a413a4e66aa3dd23c21f1252512646
Tim Luu <timbo.luu@gmail.com>
2019-04-16 14:51:34 -0700

    Add string validation

--- server/controller.js
+++ server/controller.js
@@ -1,24 +1,33 @@
-let cutString = (str) => {
-  let reducedStr = '';
-    for(let i = 0; i < str.length; i++) {
-      if(i % 3 === 2) {
-        reducedStr += str[i];
-      }
-    }
+const { validationResult } = require('express-validator/check');
 
-    return reducedStr;
+const cutString = (str) => {
+  let reducedStr = '';
+  
+  if(str.length < 3) {
+    return '(Please enter a string with 3 or more characters.)';
+  }
+
+  for(let i = 0; i < str.length; i++) {
+    if(i % 3 === 2) {
+      reducedStr += str[i];
+    }
+  }
+  
+  return reducedStr;
 }
 
 
 module.exports = {
-  cut: function(str) {
-    return new Promise((resolve, reject) => {
-      let reducedStr = cutString(str);
-      if(reducedStr || reducedStr === '') {
-        resolve(reducedStr);
-      } else {
-        reject(new Error('There appears to be an error with your string.'));
-      }
+  cut: function(req, res) {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+      return res.status(400).send(errors.array());
+    }
+
+    const str = req.body['string_to_cut'];
+    return res.status(200).send({
+      return_string: cutString(str)
     })
-  }
+  },
+  cutString
 }
